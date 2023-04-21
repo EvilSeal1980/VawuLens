49b795086efcda15ebd7ef49d9b4a5988867a509
EvilSeal1980 <evilseal09@gmail.com>
2023-04-21 09:42:17 +0530

    Added Contacts Component

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import './Contact.css'
+import React from "react";
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_2.jpg";
+import { MdEmail } from "react-icons/md";
+import { BsMessenger } from "react-icons/bs";
+import { IoLogoWhatsapp } from "react-icons/io";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <Header title="Get In Touch" image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias laborum
+        in animi sit veniam recusandae distinctio atque totam eligendi ex.
+      </Header>
+      <section className="contact">
+        <div className="container contact_container">
+          <div className="contact_wrapper">
+            <a href="mailto:evilseal09@gmail.com" target='_blank' rel="noreferrer noopener"><MdEmail/></a>
+            <a href="https://m.me/" target='_blank' rel="noreferrer noopener"><BsMessenger/></a>
+            <a href="https://wa.me/+91" target='_blank' rel="noreferrer noopener"><IoLogoWhatsapp/></a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
